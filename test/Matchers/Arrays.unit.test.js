1be9a1e31084756faba125c19b58c57c3ccd952c
hendrix <leandrotully@gmail.com>
2024-04-29 15:20:04 -0300

    7.1-)Matchers

---
+++ test/Matchers/Arrays.unit.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe("Arrays and iterables", () => {
+  
+  const shoppingList = [
+    "diapers",
+    "kleenex",
+    "trash bags",
+    "paper towels",
+    "milk",
+  ]
+
+  test("the shopping list has milk on it", () => {
+    expect(shoppingList).toContain("milk")
+    expect(new Set(shoppingList)).toContain("milk")
+  })
+})
---
+++ test/Matchers/Numbers.unit.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe("Numbers", () => {
+   
+  test("two plus two", () => {
+    const value = 2 + 2
+    expect(value).toBeGreaterThan(3)
+    expect(value).toBeGreaterThanOrEqual(3.5)
+    expect(value).toBeLessThan(5)
+    expect(value).toBeLessThanOrEqual(4.5)
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4)
+    expect(value).toEqual(4)
+  })
+
+})
